f1dff8fd82395bcbaf780b7988a99ff301b1f73d
Phu Hung Nguyen <1079742+PhuNH@users.noreply.github.com>
2019-06-14 16:23:08 +0200

    XdmfConverter can be run as a script
    Change name of layers in shapefiles
    
    Some preparation for edge collapsing

--- TuSeisSolScripts/displayh5vtk/XdmfConverter.py
+++ TuSeisSolScripts/displayh5vtk/XdmfConverter.py
@@ -11,16 +11,25 @@ from UnstructuredData import UnstructuredData
 from DataLoader import ReadHdf5Posix
 
 
-def xdmf_args_to_shp(args, output_files, which, base, scale):
+def enum(*names):
+    enums = dict(zip(names, range(len(names))))
+    return type('Enum', (), enums)
+
+
+Which = enum('VERTEX', 'CELL', 'BOTH')
+
+
+def xdmf_args_to_shp(args):
     """Uses a argparse.Namespace object
     :param args: the argparse.Namespace object
-    :param output_files: see xdmf_to_shp
-    :param which: see xdmf_to_shp
-    :param base: see xdmf_to_shp
-    :param scale: see xdmf_to_shp
     """
-    if ((which < 0 or which > 1) and len(output_files) < 2) or (0 <= which <= 1 and len(output_files) < 1):
+    if not (Which.VERTEX <= args.which <= Which.BOTH):
+        raise ValueError("args.which should be 0, 1, or 2")
+    if (args.which == Which.BOTH and len(args.outputs) < 2)\
+            or (Which.VERTEX <= args.which <= Which.CELL and len(args.outputs) < 1):
         raise ValueError("Not enough names for output files")
+
+    base = tuple(args.base)
 
     unstr = UnstructuredData()
     ReadHdf5Posix(args, unstr)
@@ -30,21 +39,24 @@ def xdmf_args_to_shp(args, output_files, which, base, scale):
     shp_driver = ogr.GetDriverByName("ESRI Shapefile")
     # Create the spatial reference, WGS84
     srs = osr.SpatialReference()
-    srs.ImportFromEPSG(32646)  # a PCS, so no lat-lon, only meter
+    srs.ImportFromEPSG(args.epsg)
+
+    xyz = unstr.xyz  # collapsed_mesh.vertices
+    connect = unstr.connect  # collapsed_mesh.faces
 
     # For points - vertices
-    if which != 1:
+    if args.which != Which.CELL:
         # Create the data source
-        point_ds = shp_driver.CreateDataSource(output_files[0])
+        point_ds = shp_driver.CreateDataSource(args.outputs[0])
         # Create the layer
-        point_layer = point_ds.CreateLayer("seis_vertices", srs, ogr.wkbPoint)
+        point_layer = point_ds.CreateLayer("vertices", srs, ogr.wkbPoint)
         # Add the fields we're interested in
         point_layer.CreateField(ogr.FieldDefn("X", ogr.OFTReal))
         point_layer.CreateField(ogr.FieldDefn("Y", ogr.OFTReal))
 
         # Process the unstructured data and add the attributes and features to the shapefile
-        for vertex in unstr.xyz:
-            coord = vertex * scale + base
+        for vertex in xyz:
+            coord = vertex * args.scale + base
             # Create the feature
             v_feature = ogr.Feature(point_layer.GetLayerDefn())
             # Set the attributes using the values from the unstructured data
@@ -68,17 +80,17 @@ def xdmf_args_to_shp(args, output_files, which, base, scale):
             v_feature = None
 
     # For triangles - cells
-    if which != 0:
-        cell_shp = output_files[0] if which == 1 else output_files[1]
+    if args.which != Which.VERTEX:
+        cell_shp = args.outputs[0] if args.which == 1 else args.outputs[1]
         triangle_ds = shp_driver.CreateDataSource(cell_shp)
-        triangle_layer = triangle_ds.CreateLayer("seis_cells", srs, ogr.wkbTriangle)
+        triangle_layer = triangle_ds.CreateLayer("cells", srs, ogr.wkbTriangle)
         triangle_layer.CreateField(ogr.FieldDefn("Data", ogr.OFTReal))
 
-        for idx, cell in enumerate(unstr.connect):
-            xyz_a, xyz_b, xyz_c = unstr.xyz[cell[0]], unstr.xyz[cell[1]], unstr.xyz[cell[2]]
-            coord_a = xyz_a * scale + base
-            coord_b = xyz_b * scale + base
-            coord_c = xyz_c * scale + base
+        for idx, cell in enumerate(connect):
+            xyz_a, xyz_b, xyz_c = xyz[cell[0]], xyz[cell[1]], xyz[cell[2]]
+            coord_a = xyz_a * args.scale + base
+            coord_b = xyz_b * args.scale + base
+            coord_c = xyz_c * args.scale + base
             c_feature = ogr.Feature(triangle_layer.GetLayerDefn())
             c_feature.SetField("Data", my_data[idx])
 
@@ -106,33 +118,55 @@ def xdmf_args_to_shp(args, output_files, which, base, scale):
     triangle_ds = None
 
 
-def xdmf_to_shp(input_file, data, idt, output_files, which=2, base=(0, 0, 0), scale=50):
+def xdmf_to_shp(input_file, data, idt, output_files=None, epsg=3857, which=Which.CELL, base=(0, 0, 0), scale=1):
     """Converts xdmf to shapefile
     :param input_file: Fault output file name (xdmf), or TODO, maybe: SeisSol netcdf (nc) or ts (Gocad)
     :param data: Data to visualize (example SRs)
     :param idt: The time step to visualize, stored in a list (1st = 0);
     -1 = all is supported by DataLoader but will give wrong results here
     :param output_files: The list of output shapefiles
-    :param which: Output shapefile for vertices (0), cells (1), or both (others)
+    :param epsg: EPSG code of the layer
+    :param which: Extract data of vertices (0), cells (1), or both (2)
     :param base: (base_x, base_y, base_z) position of the layer
     :param scale: scale of the layer
     """
-    args = Namespace(Data=[data], filename=input_file, idt=idt, oneDtMem=False, restart=[0])
-    xdmf_args_to_shp(args, output_files, which, base, scale)
+    if output_files is None:
+        if which == Which.BOTH:
+            output_files = ['output0.shp', 'output1.shp']
+        else:
+            output_files = ['output.shp']
+    args = Namespace(filename=input_file, Data=data, idt=idt, oneDtMem=False, restart=[0],
+                     outputs=output_files, epsg=epsg, which=which, base=base, scale=scale)
+    xdmf_args_to_shp(args)
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Read hdf5 fault output')
+    parser = ArgumentParser(description='Convert xdmf to shapefile')
     parser.add_argument('filename', help='fault output filename (xdmf)')
-    parser.add_argument('--Data', nargs=1, metavar='variable', default='', help='Data to visualize (example SRs)')
-    parser.add_argument('--idt', nargs='+', help='list of time step to visualize (1st = 0); -1 = all', type=int)
+    parser.add_argument('Data', nargs=1, metavar='data_name', default='', help='Data to visualize (example SRs)')
+    parser.add_argument('idt', nargs=1, help='time step to extract (1st = 0)', type=int)
     parser.add_argument('--oneDtMem', dest='oneDtMem', action='store_true', default=False,
                         help='store only the dt to be displayed in RAM')
     parser.add_argument('--restart', nargs=1, metavar='idt', help='in case of a restart at time frame idt, \
                         some postprocessing is necessary on Vr: Vr[n+idt] = Vr[n] + Vr[idt]', type=int, default=[0])
-    # args = parser.parse_args()
-    xdmf_args_to_shp(Namespace(Data=['v'], filename='../../data/sumatra-surface.xdmf', idt=[50], oneDtMem=False, restart=[0]),  # args
-                     output_files=["../../data/sumatra_cells.shp"],
-                     which=1,
-                     base=(0 * 100000, 0 * 125000, 0),
-                     scale=1)
+    parser.add_argument('--outputs', help='output shapefiles', nargs='*', default=['cells.shp'])
+    parser.add_argument('--epsg', help='EPSG code of the layer', type=int, default=3857)
+    parser.add_argument('--which', help='extract vertice data (0) or cell data (1) or both (2)',
+                        type=int, choices=[0, 1, 2], default=Which.CELL)
+    parser.add_argument('--base', help='base_x, base_y, base_z position of the layer', nargs=3,
+                        type=float, default=[0, 0, 0])
+    parser.add_argument('--scale', help='scale of the layer', type=int, default=1)
+    args = parser.parse_args()
+    # args = Namespace(filename='../../data/data-surface.xdmf',
+    #                  Data=['v3d'], idt=[160], oneDtMem=False, restart=[0],
+    #                  outputs=["../../data/seis_cells.shp"],
+    #                  epsg=3857, which=Which.CELL, base=[11.588271 * 100000, 47.751307 * 125000, 0], scale=10)
+    # args = Namespace(filename='../../data/sumatra-surface.xdmf',
+    #                  Data=['V'], idt=[50], oneDtMem=False, restart=[0],
+    #                  outputs=['../../data/sumatra_cells.shp'],
+    #                  epsg=32646, which=Which.CELL, base=[0, 0, 0], scale=1)
+    xdmf_args_to_shp(args)
+    # xdmf_to_shp(input_file='../../data/sumatra-surface.xdmf',
+    #             data=['V'], idt=[50],
+    #             output_files=['../../data/sumatra_cells.shp'],
+    #             epsg=32646, which=Which.CELL)
